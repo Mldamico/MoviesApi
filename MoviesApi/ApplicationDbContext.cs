26cbf5e52a9f406afdc671096b87c762805aac7e
mldamico <matias.lioneldamico@gmail.com>
2023-05-23 16:28:28 -0300

    Genre and applicationDbContext setup

---
+++ MoviesApi/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.Entities;
+
+namespace MoviesApi;
+
+public class ApplicationDbContext:DbContext
+{
+    public ApplicationDbContext(DbContextOptions options):base(options)
+    {
+        
+    }
+
+    public DbSet<Genre> Genres { get; set; }
+}
